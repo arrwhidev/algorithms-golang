461c133868a2c6a062cfc218913123750fef5fe7
arran <arrwhidev@gmail.com>
2017-09-09 16:05:03 +0100

    Added description to linear search algorithm

--- search/linear.go
+++ search/linear.go
@@ -1,5 +1,13 @@
 package search
 
+/**
+Linear Search
+
+- Go through entire array until item is found.
+- Worst case: O(n)
+- Best case: Ω(1)
+*/
+
 func Linear(array []int, target int) int {
 	indexOfTarget := -1
 	for i, el := range array {
